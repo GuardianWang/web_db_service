c9ec95ad2e4cc69779c636dd4d29a781fff7babd
guardianwang <962247283@qq.com>
2022-12-16 19:38:00 -0500

    docker

--- README.md
+++ README.md
@@ -21,6 +21,20 @@ connect("localhost/students")
 >db.students.find()
 ```
 
+## Docker
+
+build docker image
+```
+docker build --env-file .env.docker -t studentdb .
+```
+
+start containers
+```
+# remember to use name mongo, the container name acts as hostname
+docker run --network studentdb-network -p 27017:27017 -d --name mongo mongo
+docker run --network studentdb-network -p 3000:3000 -d --env-file .env.docker studentdb
+```
+
 ## Reference
 
 - [https://www.youtube.com/watch?v=fgTGADljAeg]()
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+  }
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const studentsRouter = require('./routes/students');
 
 const app = express();
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", error => console.log(error));
 db.once("open", () => console.log("connected to mongodb"));
