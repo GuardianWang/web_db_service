cb89731ff6051eb8902ff95aa6a1d5efde5afeb4
guardianwang <962247283@qq.com>
2022-12-26 03:11:46 -0500

    don't specify mapping host port

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ const { default: mongoose } = require("mongoose");
 const studentsRouter = require('./routes/students');
 
 const app = express();
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true, 
-    useFindAndModify: false
-});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true,});
 const db = mongoose.connection;
 db.on("error", error => console.log(error));
 db.once("open", () => console.log("connected to mongodb"));
